bdf9ca91d279c898bf9bd0ce96a10ff7517bd045
0NancyH <101585784@georgebrown.ca>
2024-12-11 04:36:10 +0000

    nav links properly linked to proper pages

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,16 +7,16 @@ export default function Nav() {
         <div className="navcontainer">
             <ul className="nav justify-content-center">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <a className="nav-link active" aria-current="page" href="./">Home</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link" href="./about">About</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/shop">Shop</a>
+                    <a className="nav-link" href="./shop">Shop</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/contact">Contact</a>
+                    <a className="nav-link" href="./contact">Contact</a>
                 </li>
             
             </ul>
